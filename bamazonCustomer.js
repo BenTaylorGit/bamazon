d2c384cf9ca046aba4fce26a02bd493b4937da0c
Ben Taylor <bentaylor@knights.ucf.edu>
2020-03-19 16:14:22 -0400

    added require vars and started connection to mySQL DB

---
+++ bamazonCustomer.js
@@ -0,0 +1,12 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
+});
+
